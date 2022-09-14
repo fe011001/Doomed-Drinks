a1c843218e288a0eb7d27d57d159b282b9baa316
Jon Kelly <jonfkelly@gmail.com>
2022-09-14 14:10:02 -0500

    alcohol api functions for merge

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,12 @@
 let drinkName = "whiskey"
 let glassType = "Cocktail_glass"
-let drinkType = "shake"
+let drinkType = "shot"
 
+let spiritSelector = document.querySelector("#spiritSelector");
+let glassSelector = document.querySelector("#glassSelector");
+let typeSelector = document.querySelector("#typeSelector");
+
+let drinkOptions;
 
 const options = {
     method: 'GET',
@@ -13,37 +18,42 @@ const options = {
 
 fetch(`https://the-cocktail-db.p.rapidapi.com/search.php?i=${drinkName}`, options)
     .then(response => response.json())
-    .then(response => console.log(response))
+    .then(response => console.log("drinkName", response))
     .catch(err => console.error(err));
 
 fetch(`https://the-cocktail-db.p.rapidapi.com/filter.php?g=${glassType}`, options)
     .then(response => response.json())
-    .then(response => console.log(response))
+    .then(response => console.log("glassType", response))
     .catch(err => console.error(err));
 
 fetch(`https://the-cocktail-db.p.rapidapi.com/filter.php?c=${drinkType}`, options)
     .then(response => response.json())
-    .then(response => console.log(response))
+    .then(response => console.log("drinkType", response))
     .catch(err => console.error(err));
 
 fetch(`https://the-cocktail-db.p.rapidapi.com/list.php?i=list`, options)
     .then(response => response.json())
-    .then(response => console.log(response))
-    .catch(err => console.error(err));
+    .then(response => console.log("ingredient list", response))
+    .catch(err => console.error(err))
 
+    
+// console.log(drinkOptions)
+
+// for (var i = 0; i < drinkOptions.length; i++) {
+
+//     var opt = document.createElement("option");
+//     opt.textContent = drinkOptions;
+//     opt.setAttribute("data-index", i);
+
+//     spiritSelector.appendChild(opt);
+// }
 
 fetch(`https://the-cocktail-db.p.rapidapi.com/list.php?g=list`, options)
     .then(response => response.json())
-    .then(response => console.log(response))
+    .then(response => console.log("glass list", response))
     .catch(err => console.error(err));
 
 fetch(`https://the-cocktail-db.p.rapidapi.com/list.php?c=list`, options)
     .then(response => response.json())
-    .then(response => console.log(response))
+    .then(response => console.log("cocktail type list", response))
     .catch(err => console.error(err));
-
-
-
-// fetch(`https://api.nasa.gov/neo/rest/v1/neo/browse?api_key=DEMO_KEY`)
-// .then(response => response.json())
-// .then(response => console.log(response))
